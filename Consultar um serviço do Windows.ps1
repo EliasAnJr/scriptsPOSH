df5d66e2f1bc4b15ef3f1f1c916d0b58f7a30231
EliasAnJr <111619033+EliasAnJr@users.noreply.github.com>
2022-11-15 22:09:04 -0300

    Add files via upload
    Adicionado script

---
+++ Consultar um serviço do Windows.ps1
@@ -0,0 +1,12 @@
+﻿#Consultar um serviço do Windows
+Clear-Host
+$servico = Get-Process -Name AnyDesk
+$servico
+
+If($servico.Status -eq "Runnig") {Write-Host "Serviço em Execução"}
+    pause
+    
+    #Else
+    #{
+   # Write-Host "Serviço Parado"
+    #}
